56ada6571aaefe5ecbd8303910167beb03916cd9
lesajayaweera <lesanduluthmeen8@gmail.com>
2025-07-30 21:41:55 +0530

    previous state

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -3,6 +3,7 @@ import 'package:sport_ignite/pages/home.dart';
 import 'package:sport_ignite/pages/veiwAthletes.dart';
 import 'package:sport_ignite/widget/common/appbar.dart';
 import 'package:sport_ignite/widget/common/bottomNavigation.dart';
+import 'package:sport_ignite/widget/common/post_create.dart';
 
 class Dashboard extends StatefulWidget {
   final String role;
@@ -13,6 +14,8 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  
+
   int _currentIndex = 0;
 
   @override
@@ -20,30 +23,33 @@ class _DashboardState extends State<Dashboard> {
     final List<Widget> athleteScreens = [
       SocialFeedScreen(role: widget.role),
       Center(child: Text('Athlete My Network')),
-      Center(child: Text('Athlete Post')),
+      SharePostSheet(scrollController: ScrollController()),
       Center(child: Text('Athlete Shorts')),
       Center(child: Text('Athlete Sponsorships')),
     ];
     final List<Widget> sponsorScreens = [
       SocialFeedScreen(role: widget.role),
+
       Center(child: Text('My Network')),
-      Center(child: Text('Post')),
+      SharePostSheet(scrollController: ScrollController()),
       Center(child: Text('Shorts')),
-      AthletesScreen(role: widget.role)
+      AthletesScreen(role: widget.role),
     ];
 
     final screens = widget.role == 'Athlete' ? athleteScreens : sponsorScreens;
     return Scaffold(
       backgroundColor: const Color(0xFFF5F5F5),
-      appBar: LinkedInAppBar(page: false, role: widget.role),
+      appBar: LinkedInAppBar(page: false, role: widget.role,),
       body: screens[_currentIndex],
       bottomNavigationBar: CustomBottomNavigation(
         currentIndex: _currentIndex,
         role: widget.role,
         onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
+          
+            setState(() {
+              _currentIndex = index;
+            });
+          
         },
       ),
     );
