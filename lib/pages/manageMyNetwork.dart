9ec6a9ba059407cdb175d02abbc48a1c7b70ee15
lesajayaweera <lesanduluthmeen8@gmail.com>
2025-08-07 07:45:58 +0530

    commited once again cus  i have trust issues

--- lib/pages/manageMyNetwork.dart
+++ lib/pages/manageMyNetwork.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sport_ignite/pages/manageInvititations.dart';
-
+// hello worild
 class NetworkManagementScreen extends StatefulWidget {
   final String role;
   const NetworkManagementScreen({Key? key, required this.role})
